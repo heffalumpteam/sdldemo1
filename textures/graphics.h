56b51de003e932bc56c81d1fa6e540f7f2a760cc
heffalumpteam <tb15269@my.bristol.ac.uk>
2015-10-18 12:45:07 +0100

    Added Textures example

---
+++ textures/graphics.h
@@ -0,0 +1,1 @@
+void graphics_init(void);
---
+++ textures/main.c
@@ -0,0 +1,9 @@
+/*gcc main.c -o test -Wall -Wextra -pedantic -std=c90 -lSDL2*/
+#include "graphics.h"
+
+void graphics_init(void);
+
+int main (){
+  graphics_init();
+  return 0;
+}
